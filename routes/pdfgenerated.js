edf31082afa431a0f32f1571690d07105ce311a3
Jonathan Weyermann <jonathan.weyermann@gov.ab.ca>
2023-04-04 10:24:23 -0600

    first commit

---
+++ routes/pdfgenerated.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET uptime listing. */
+router.get('/', function(req, res, next) {
+
+  console.log(JSON.stringify(res, getCircularReplacer()) + "<res");
+  console.log(JSON.stringify(req, getCircularReplacer()) + "<req");
+  res.send('respond with a uptime');
+});
+
+/* POST uptime listing. */
+router.post('/', function(req, res, next) {
+
+  //console.log(JSON.stringify(res, getCircularReplacer()) + "<res");
+  //console.log(JSON.stringify(req, getCircularReplacer()) + "<req");
+  console.log(JSON.stringify(new Date().toTimeString()) + "- PDF Data:" + JSON.stringify(req?.body, null, 2))
+  res.send('A PDF has been generated');
+});
+
+module.exports = router;
+
+
+const getCircularReplacer = () => {
+  const seen = new WeakSet();
+  return (key, value) => {
+    if (typeof value === "object" && value !== null) {
+      if (seen.has(value)) {
+        return;
+      }
+      seen.add(value);
+    }
+    return value;
+  };
+};
